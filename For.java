9e212988b8322738232ca006518b72ecc354ff96
DoHuynhNhu <nhub2014594@student.ctu.edu.vn>
2023-10-17 17:05:46 +0700

    them chuong trinh for.java

---
+++ For.java
@@ -0,0 +1,9 @@
+package B2014594_DoHuynhNhu_CT300_B1;
+
+public class For {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
